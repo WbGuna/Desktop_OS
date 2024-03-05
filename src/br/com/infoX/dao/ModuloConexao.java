3b932b9e5cf3ff8b6f5d47b9bf5de010c1e96867
Willian <wb.premiertech@gmail.com>
2024-03-05 07:02:07 -0300

    Conexão

--- src/br/com/infoX/dao/ModuloConexao.java
+++ src/br/com/infoX/dao/ModuloConexao.java
@@ -12,7 +12,7 @@ public class ModuloConexao {
         String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/dbinfox";
         String user = "root";
-        String password = "will@1844";
+        String password = "root";
         try {
             Class.forName(driver);
             conexao = DriverManager.getConnection(url, user, password);
